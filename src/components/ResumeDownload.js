1b562f88c44a78cf1f19b156dbb4e5ac82a483a4
Naitik Lodha <69336744+naitik-lodha@users.noreply.github.com>
2023-12-27 19:29:27 +0530

    Update ResumeDownload.js

--- src/components/ResumeDownload.js
+++ src/components/ResumeDownload.js
@@ -4,7 +4,7 @@ const ResumeDownload = () => {
     <>
       {" "}
       <button className="bg-gradient-to-tr p-2 text-white top-0 px-4 rounded-md from-pink-600 to-purple-500">
-        <a className="flex" href="NAITIK LODHA.pdf" title="" download>
+        <a className="flex" href="NAITIK_LODHA.pdf" title="" download>
           Resume
           <FiDownload size={20} color="white" />
         </a>
